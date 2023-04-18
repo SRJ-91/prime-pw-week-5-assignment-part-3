c146f3074d76cd01daccdd3b7ff659d4d302df3e
SRJ-91 <Sawyerjen@hotmail.com>
2023-04-18 15:33:22 -0500

    checkpoint for the day

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -8,12 +8,12 @@
   - Create a new object having the above properties
   - Add the new object to the end of the `collection` array
   - Return the newly created object
-
+COMPLETE
 - Test the `addToCollection` function:
   - Add 6 albums to your collection. Aim to have a mix of both same and different artists and published years. (Feel free to share your musical interests, or make stuff up. Totally fine either way.)
   - Console.log each album as added using the returned value.
   - After all are added, console.log the `collection` array.
-
+COMPLETE
 - Add a function named `showCollection`. This function should:
   - Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
   - Console.log the number of items in the array.
@@ -88,5 +88,21 @@ addToCollection('Back In Black', 'AC/DC', 1980)
 
 addToCollection('Appetite For Destruction', 'Guns N Roses', 1987)
 
-
 console.log(collection);
+
+function showCollection(array) {
+    console.log(array.length);
+    for (let i = 0; i < array.length; i++) {
+        console.log(title, 'by ', artist, ' published in ', yearPublished);
+    }
+}
+
+showCollection(collection);
+/*
+- Add a function named `showCollection`. This function should:
+  - Take in an array parameter. (This allows it to be reused to show any collection, like the results from the find or search.)
+  - Console.log the number of items in the array.
+  - Loop over the array and console.log each album's information formatted like: `TITLE by ARTIST, published in YEAR`.
+
+- Test the `showCollection` function.
+*/
